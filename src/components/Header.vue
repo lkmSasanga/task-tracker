93200e1b7463b5bb581b0d94284338cfa66e476f
lkmSasanga <lkmsasanga@gmail.com>
2021-07-20 23:08:09 +0530

    header added

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button class="btn"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
