7f0c06c6e21de80ffb0bcb7c865e0d7060eba785
lkmSasanga <lkmsasanga@gmail.com>
2021-07-21 12:22:56 +0530

    toggle reminder added

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,75 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker"/>
-    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
+    <Header title="Task Tracker" />
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Tasks from './components/Tasks.vue'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Tasks,
   },
   data() {
     return {
-      tasks: []
-    }
+      tasks: [],
+    };
   },
   methods: {
     deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id)
-    }
+      if (confirm("Are you sure?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
       {
         id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2.30pm',
-        reminder: true
+        text: "Doctors Appointment",
+        day: "March 1st at 2.30pm",
+        reminder: true,
       },
       {
         id: 2,
-        text: 'Person 2 Appointment',
-        day: 'March 1st at 2.30pm',
-        reminder: false
+        text: "Person 2 Appointment",
+        day: "March 1st at 2.30pm",
+        reminder: false,
       },
       {
         id: 3,
-        text: 'Person 3 Appointment',
-        day: 'March 1st at 2.30pm',
-        reminder: true
+        text: "Person 3 Appointment",
+        day: "March 1st at 2.30pm",
+        reminder: true,
       },
-    ]
-  }
-}
+    ];
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
