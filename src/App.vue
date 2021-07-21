d2a91b690f671a289de723d2ce4892b57891e9c2
lkmSasanga <lkmsasanga@gmail.com>
2021-07-21 09:44:22 +0530

    tasks added & fontawesome cdn

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="container">
     <Header title="Task Tracker"/>
+    <Tasks :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2.30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Person 2 Appointment',
+        day: 'March 1st at 2.30pm',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Person 3 Appointment',
+        day: 'March 1st at 2.30pm',
+        reminder: true
+      },
+    ]
   }
 }
 </script>
